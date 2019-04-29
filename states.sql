36ccbcf489201d6da0b36f71d93b4d981a6ffe8a
Satish Dhiman <satish.dhiman025@gmail.com>
2019-04-30 00:57:31 +0530

    duplicate id of State is fixed

--- states.sql
+++ states.sql
@@ -4108,8 +4108,8 @@ INSERT INTO `states` (`id`, `name`, `country_id`) VALUES
 (4118, 'Matabeleland North', 246),
 (4119, 'Matabeleland South', 246),
 (4120, 'Midlands', 246),
-(4121, 'FCT', 160),
-(4122, 'Nasarawa',160);
+(4122, 'FCT', 160),
+(4123, 'Nasarawa',160);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
